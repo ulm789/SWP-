896435387509faa643fda21e077545f23b0c3949
ulm789 <ulm789@gmail.com>
2020-06-29 10:37:10 +0900

    add Sum and Mul

---
+++ calc/calc.py
@@ -0,0 +1,18 @@
+from cgi import parse_qs
+from template import html
+
+def application(environ, start_response):
+    d = parse_qs(environ['QUERY_STRING'])
+    num1 = d.get('num1', [''])[0]
+    num2 = d.get('num2', [''])[0]
+    sum, mul = 0, 0
+    if '' not in [num1, num2]:
+        num1, num2 = int(num1), int(num2)
+        sum = num1 + num2
+        mul = num1 * num2
+    response_body = html % {'sum':sum, 'mul':mul}
+    start_response('200 OK', [
+        ('Content-Yype', 'text/html'),
+        ('Content-Length', str(len(response_body)))
+    ])
+    return [response_body]
---
+++ calc/template.py
@@ -0,0 +1,13 @@
+html = """
+<html>
+    <body>
+        <form action="">
+                num1 = <input type = "number" name = "num1"><br><br>
+                num2 = <input type = "number" name = "num2">
+            <input type="submit">
+        </form>
+        Sum = %(sum)d <br><br>
+        Mul = %(mul)d
+    </body>
+</html>
+"""
