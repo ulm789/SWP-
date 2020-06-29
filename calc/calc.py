5e1242686634934efb201f670d504bfcecc108fc
ulm789 <ulm789@gmail.com>
2020-06-29 10:48:34 +0900

    issue #5

--- calc/calc.py
+++ calc/calc.py
@@ -5,7 +5,7 @@ def application(environ, start_response):
     d = parse_qs(environ['QUERY_STRING'])
     num1 = d.get('num1', [''])[0]
     num2 = d.get('num2', [''])[0]
-    sum, mul = 0, 0
+    sum, mul = 4, 26
     if '' not in [num1, num2]:
         num1, num2 = int(num1), int(num2)
         sum = num1 + num2
